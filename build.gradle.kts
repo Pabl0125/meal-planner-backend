95c1b9d7ceff866602cf9bf367ada9cdddf7964a
Pablo Araujo <pabloarro2005@gmail.com>
2026-08-18 16:38:43 +0200

    modified dependencies file

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ description = "meal-planner-backend"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(25)
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
